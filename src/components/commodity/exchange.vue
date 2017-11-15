4c23c0a5556a9b81528124cc093e66c859bccf04
unknown <wujing@GSL-YANFA-095.AladingBank.com>
2017-11-15 18:25:27 +0800

    兑换页面 填写个人信息  兑换成功页面

---
+++ src/components/commodity/exchange.vue
@@ -0,0 +1,48 @@
+<template>  
+  <div class="exchangeContent">
+      <ul class="inputInfo">
+        <li class="normalInput"><p>姓名</p><input type="text" placeholder="请输入收货人姓名" ></li>
+        <li class="normalInput"><p>电话</p><input type="text" placeholder="请输入收货人联系电话" ></li>
+        <li class="normalInput remakes"><p>备注</p><textarea type="text" placeholder="请输入收货人地址/兑换平台账号" ></textarea> </li>
+        <li class="goodsInfo">
+          <img src="../../assets/images/icon.jpg">
+          <div>
+              <p>爱奇艺会员一个月爱奇艺会员一个月</p>
+              <p ><span>兑换数量：2</span><span>支付积分：40</span></p>
+          </div>
+        </li>
+        <li class="delivery"> <span>配送方式</span> <span>包邮</span></li>
+      </ul>
+      <div class="onsure">
+          <p>温馨提示：确认兑换后不可取消</p>
+          <button v-on:click='isSuccess()'>确认兑换</button>
+      </div>
+  </div>
+
+</template>
+
+<style scoped >
+ @import '../../assets/style/homeAndPrize.css';
+</style>
+
+<script type="text/babel">
+  export default {
+    name: 'hello1',
+    data() {
+       return{
+         
+       }
+      },
+    mounted() {
+
+    },
+    methods: {
+        isSuccess(){
+          this.$router.push({path:'/commodity/exchangeSuccess'});
+        }
+    },  
+    watch: {
+     
+      },
+  }
+</script>
---
+++ src/components/commodity/exchangeSuccess.vue
@@ -0,0 +1,41 @@
+<template>  
+  <div class="exchangeSuccessContent">
+      <ul>
+        <li>
+            <img src="../../assets/images/exchangeSuccess.png">
+            <p>兑换成功</p>
+            <p class="residue">剩余可用积分:20</p>
+        </li>
+        <li class="twoButton">
+            <button>查看兑换记录</button>
+            <button>返回活动主页</button>
+        </li>
+        <li ><button class="share">分享给好友</button></li>
+      </ul>
+  </div>
+
+</template>
+
+<style scoped >
+ @import '../../assets/style/homeAndPrize.css';
+</style>
+
+<script type="text/babel">
+  export default {
+    name: 'hello1',
+    data() {
+       return{
+         
+       }
+      },
+    mounted() {
+
+    },
+    methods: {
+     
+    },
+    watch: {
+     
+      },
+  }
+</script>
