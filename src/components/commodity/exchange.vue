39d020abe422e05f8aecdd3074e0d464098c9273
unknown <wujing@GSL-YANFA-095.AladingBank.com>
2017-11-17 15:31:02 +0800

    增加手机号校验

--- src/components/commodity/exchange.vue
+++ src/components/commodity/exchange.vue
@@ -2,7 +2,7 @@
   <div class="exchangeContent">
       <ul class="inputInfo">
         <li class="normalInput"><p>姓名</p><input type="text" placeholder="请输入收货人姓名" ></li>
-        <li class="normalInput"><p>电话</p><input type="text" placeholder="请输入收货人联系电话" ></li>
+        <li class="normalInput"><p>电话</p><input type="text" placeholder="请输入收货人联系电话" v-model='phoneNum'></li>
         <li class="normalInput remakes"><p>备注</p><textarea type="text" placeholder="请输入收货人地址/兑换平台账号" ></textarea> </li>
         <li class="goodsInfo">
           <img src="../../assets/images/icon.jpg">
@@ -15,7 +15,7 @@
       </ul>
       <div class="onsure">
           <p>温馨提示：确认兑换后不可取消</p>
-          <button v-on:click='isSuccess()'>确认兑换</button>
+          <button v-on:click='isSure()'>确认兑换</button>
       </div>
   </div>
 
@@ -26,20 +26,34 @@
 </style>
 
 <script type="text/babel">
+import { Toast } from 'mint-ui'
   export default {
     name: 'hello1',
     data() {
        return{
-         
+         phoneNum:''
        }
       },
     mounted() {
 
     },
     methods: {
-        isSuccess(){
-          this.$router.push({path:'/commodity/exchangeSuccess'});
-        }
+      isSure(){
+          var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
+          if(myreg.test(this.phoneNum)){
+             this.$router.push({path:'/commodity/exchangeSuccess'});
+           }else{
+              this.alertMessage('请输入合法的号码');
+           } 
+         
+      },
+      alertMessage(message){
+          Toast({
+              message: message,
+              position: 'center',
+              duration: 1000
+            });
+      }
     },  
     watch: {
      
